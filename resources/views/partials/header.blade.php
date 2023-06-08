cbcde9bd47410b331b3e8da64809bb2a9e562a01
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-08 17:00:18 +0200

    set page controller

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,15 @@
-<header class="container-fluid">
-  <ul class="nav">
+<header class="container-fluid bg-black">
+  <ul class="nav my-2">
 
     <li class="nav-item">
-      <a class="nav-link" href="{{route('home')}}">home</a>
+      <a class="nav-link {{Route::currentRouteName() === 'home' ? 'active' : '' }}" href="{{route('home')}}">home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="{{route('movie')}}">movie</a>
+      <a class="nav-link {{Route::currentRouteName() === 'movie' ? 'active' : '' }}" href="{{route('movie')}}">movie</a>
     </li>
     {{-- <li class="nav-item">
       <a class="nav-link disabled">Disabled</a>
     </li> --}}
+    {{-- <h3>{{Route::currentRouteName()}}</h3> --}}
   </ul>
 </header>
